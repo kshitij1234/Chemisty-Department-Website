7e99425e3a81bc02d1042ad09c8f06f835c2fcab
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-15 02:04:35 +0530

    added view for faculty list

--- PeopleApp/views.py
+++ PeopleApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Faculty
 # Create your views here.
+
+def faculty_list(request):
+
+    faculty = Faculty.objects.all().order_by('name')
+
+    return render(request, 'PeopleApp/FacultyList.html', {'faculty': faculty})
