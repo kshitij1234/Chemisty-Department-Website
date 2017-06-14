e93729387e2208754d0cb07baf4da75be8745678
Abhishek Agrawal <aagrawal207@gmail.com>
2017-06-15 02:32:42 +0530

    Solved static path bug

--- PeopleApp/views.py
+++ PeopleApp/views.py
@@ -3,7 +3,5 @@ from .models import Faculty
 # Create your views here.
 
 def faculty_list(request):
-
     faculty = Faculty.objects.all().order_by('name')
-
     return render(request, 'PeopleApp/FacultyList.html', {'faculty': faculty})
