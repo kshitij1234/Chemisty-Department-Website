f3937ca0c74f71ffa3552b25113bae20a19adc18
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-19 01:57:43 +0530

    made staff model

--- PeopleApp/models.py
+++ PeopleApp/models.py
@@ -50,3 +50,40 @@ class Faculty(models.Model):
 
     def get_image_path(self):
         return str(self.profile_picture.url)[16:]
+
+class Staff(models.Model):
+
+    name = models.CharField(max_length=100, blank=False)
+    designation = models.ForeignKey('Designations')
+    email = models.EmailField(primary_key=True)
+    phone = models.CharField(max_length=12)
+    profile_picture = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        # object is being removed from db, remove the file from storage first
+        shutil.rmtree(os.path.join("PeopleApp", "static", "UserImages", type(self).__name__, str(self.pk)))
+        return super(Staff, self).delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        # object is possibly being updated, if so, clean up.
+        self.remove_on_image_update()
+        return super(Staff, self).save(*args, **kwargs)
+
+    def remove_on_image_update(self):
+        try:
+            # is the object in the database yet?
+            obj = Staff.objects.get(pk=self.pk)
+        except Staff.DoesNotExist:
+            # object is not in db, nothing to worry about
+            return
+        # is the save due to an update of the actual image file?
+        if obj.profile_picture and self.profile_picture and obj.profile_picture != self.profile_picture:
+            # delete the old image file from the storage in favor of the new file
+            obj.profile_picture.delete()
+
+    def get_image_path(self):
+        return str(self.profile_picture.url)[16:]
--- PeopleApp/urls.py
+++ PeopleApp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^faculty/$', views.faculty_list, name='faculty_list')
+    url(r'^faculty/$', views.faculty_list, name='faculty_list'),
+    url(r'^staff/$', views.staff_list, name='staff_list'),
 ]
