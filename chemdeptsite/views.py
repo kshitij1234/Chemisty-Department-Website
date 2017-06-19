b2efc31204e9aaededbdd62bc51645086fedf371
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-19 14:52:01 +0530

    save changes

--- chemdeptsite/views.py
+++ chemdeptsite/views.py
@@ -6,6 +6,6 @@ def index(request):
     events = Events.objects.all().order_by("-date")
     news = News.objects.all().order_by("-date")
     research = Research.objects.all().order_by("-date")
-    quicklinks = QuickLinks.objects.all().order_by("-date")
+    quicklinks = QuickLinks.objects.all().order_by("date")
     object_list = {'notice': notice, 'events': events, 'news': news, 'research': research, 'quicklinks': quicklinks}
     return render(request, 'home.html', object_list)
