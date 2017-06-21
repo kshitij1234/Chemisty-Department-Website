45d94d16fd62ab6d995efe511f2edd3440b6cfb4
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-22 02:20:54 +0530

    added model for phd students

--- PeopleApp/models.py
+++ PeopleApp/models.py
@@ -141,3 +141,49 @@ class MscStudents(models.Model):
 
     def get_image_path(self):
         return str(self.profile_picture.url)[16:]
+
+
+class PhdStudents(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    batch = models.ForeignKey('Batch')
+    email = models.CharField(primary_key=True, max_length=50, blank=False)
+    profile_picture = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
+    # At first thought of making it as ForeignKey of Faculty model but what if a faculty leaves
+    # and this particular student has already passed out ?
+    # We would want to delete the faculty from our Faculty database but at the same time would want
+    # to show the faculty as supervisor of this student.
+    # It is only possible if this is an independent field.
+
+    supervisor = models.CharField(max_length=50, blank=True, null=True)
+    research = models.TextField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        # object is being removed from db, remove the file from storage first
+        shutil.rmtree(os.path.join("PeopleApp", "static", "UserImages", type(self).__name__, str(self.pk)))
+        return super(PhdStudents, self).delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        # object is possibly being updated, if so, clean up.
+        self.remove_on_image_update()
+        return super(PhdStudents, self).save(*args, **kwargs)
+
+    def remove_on_image_update(self):
+        try:
+            # is the object in the database yet?
+            obj = PhdStudents.objects.get(pk=self.pk)
+        except PhdStudents.DoesNotExist:
+            # object is not in db, nothing to worry about
+            return
+        # is the save due to an update of the actual image file?
+        if obj.profile_picture and self.profile_picture and obj.profile_picture != self.profile_picture:
+            # delete the old image file from the storage in favor of the new file
+            obj.profile_picture.delete()
+
+    def get_image_path(self):
+        return str(self.profile_picture.url)[16:]
+
+
