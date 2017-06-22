d19886098dada0301c399fc690bc232e5291e67c
Abhishek Agrawal <aagrawal207@gmail.com>
2017-06-23 00:55:17 +0530

    New home page is being made.

--- chemdeptsite/views.py
+++ chemdeptsite/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import NoticeBoard, News, Research, QuickLinks
+from Events.models import Events
 
 def index(request):
     notice = NoticeBoard.objects.all().order_by("-date")
     news = News.objects.all().order_by("-date")
     research = Research.objects.all().order_by("-date")
     quicklinks = QuickLinks.objects.all().order_by("date")
-    object_list = {'notice': notice, 'news': news, 'research': research, 'quicklinks': quicklinks}
+    events = Events.objects.all().order_by("date")
+    object_list = {'notice': notice, 'news': news, 'research': research, 'quicklinks': quicklinks, 'events': events}
     return render(request, 'home.html', object_list)
 
 def facilities(request):
