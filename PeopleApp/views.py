6e06a2c798b4530ae5e828c59860688a233bb7e2
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-23 11:22:51 +0530

    added all ug data

--- PeopleApp/views.py
+++ PeopleApp/views.py
@@ -18,11 +18,12 @@ def undergraduate_list(request):
     students13 = UndergraduateStudents.objects.filter(batch__batch__contains="2013").order_by('rollno')
     students14 = UndergraduateStudents.objects.filter(batch__batch__contains="2014").order_by('rollno')
     students15 = UndergraduateStudents.objects.filter(batch__batch__contains="2015").order_by('rollno')
-    students16 = UndergraduateStudents.objects.filter(batch__batch__contains="2016").order_by('rollno')
+    # students16 = UndergraduateStudents.objects.filter(batch__batch__contains="2016").order_by('rollno')
     return render(request, 'PeopleApp/UndergraduateList.html', {'students13': students13,
                                                                 'students14': students14,
                                                                 'students15': students15,
-                                                                'students16': students16})
+                                                                #             'students16': students16
+                                                                })
 
 
 def msc_list(request):
