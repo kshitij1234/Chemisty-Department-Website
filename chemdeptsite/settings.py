8a6a2584fe6ff6a154b85b98cfd6d53d9be31fa4
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-06-23 22:06:49 +0530

    added support for pythonanywhere

--- chemdeptsite/settings.py
+++ chemdeptsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6cn1lx7zo_$h647$5n8vt$k5j&4rc9wqt@++)*305_*_8@ppy$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'chemiitp.pythonanywhere.com']
 
 
 # Application definition
