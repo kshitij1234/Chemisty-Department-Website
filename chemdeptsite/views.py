79a5aa237164e6e04f3b220322fe03a97befcf48
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-07-11 00:51:17 +0530

    added research highlights

--- chemdeptsite/views.py
+++ chemdeptsite/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import NoticeBoard, News, Research, QuickLinks
+from .models import NoticeBoard, News, QuickLinks
+from research.models import ResearchAreas
 from Events.models import Events
+
 
 def index(request):
     notice = NoticeBoard.objects.all().order_by("-date")
     news = News.objects.all().order_by("-date")
-    research = Research.objects.all().order_by("-date")
+    research = ResearchAreas.objects.all().order_by("title")
     quicklinks = QuickLinks.objects.all().order_by("date")
     events = Events.objects.all().order_by("date")
     object_list = {'notice': notice, 'news': news, 'research': research, 'quicklinks': quicklinks, 'events': events}
