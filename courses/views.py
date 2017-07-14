351a1ee5d2308ffafd2d341c560423f39a6c3fa0
Abhishek Agrawal <aagrawal207@gmail.com>
2017-07-15 02:59:10 +0530

    Indentation improved on individual faculty page

--- courses/views.py
+++ courses/views.py
@@ -3,10 +3,10 @@ from .models import BtechCourses
 
 def btech_courses(request):
     btech_courses = BtechCourses.objects.all().order_by("id")
-    return render(request, 'btech_courses.html', {'btech_courses' : btech_courses})
+    return render(request, 'error_404.html', {'btech_courses' : btech_courses})
 
 def msc_courses(request):
-    return render(request, 'msc_courses.html')
+    return render(request, 'error_404.html')
 
 def phd_courses(request):
-    return render(request, 'phd_courses.html')
+    return render(request, 'error_404.html')
