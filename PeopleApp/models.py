0f6f00b2625828a76efe0e11c395ba2f68b17975
Abhishek Agrawal <aagrawal207@gmail.com>
2017-07-14 22:42:27 +0530

    data added..

--- PeopleApp/models.py
+++ PeopleApp/models.py
@@ -29,7 +29,7 @@ class Faculty(models.Model):
     list_position = models.IntegerField(default=1)
     phd = models.CharField(max_length=100, blank=True, null=True)
     fax = models.CharField(max_length=20, blank=True, null=True)
-    google_scholar = models.CharField(max_length=200, default="#")
+    google_scholar = models.CharField(max_length=200, blank=True, null=True)
     professional_experience = models.TextField(blank=True, null=True)
     awards_honors = models.TextField(blank=True, null=True)
     publications = models.TextField(blank=True, null=True)
@@ -38,6 +38,7 @@ class Faculty(models.Model):
     conference_attended = models.TextField(blank=True, null=True)
     sponsored_projects = models.TextField(blank=True, null=True)
     teaching = models.TextField(blank=True, null=True)
+    patents = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
