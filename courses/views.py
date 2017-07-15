403f555323db45b13e42601386183468f1d95595
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-07-15 18:30:35 +0530

    some changes

--- courses/views.py
+++ courses/views.py
@@ -3,7 +3,7 @@ from .models import BtechCourses
 
 def btech_courses(request):
     btech_courses = BtechCourses.objects.all().order_by("id")
-    return render(request, 'error_404.html', {'btech_courses' : btech_courses})
+    return render(request, 'btech_courses.html', {'btech_courses' : btech_courses})
 
 def msc_courses(request):
     return render(request, 'error_404.html')
