761bf55493589f67b0727de3f72b3d7dd30d76e3
Abhishek Agrawal <aagrawal207@gmail.com>
2017-07-15 21:36:57 +0530

    All the courses info is added

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import BtechCourses
+from .models import BtechCourses, MscCourses, PhdCourses
 
 def btech_courses(request):
     btech_courses = BtechCourses.objects.all().order_by("id")
     return render(request, 'btech_courses.html', {'btech_courses' : btech_courses})
 
 def msc_courses(request):
-    return render(request, 'error_404.html')
+    msc_courses = MscCourses.objects.all().order_by("id")
+    return render(request, 'msc_courses.html', {'msc_courses' : msc_courses})
 
 def phd_courses(request):
-    return render(request, 'error_404.html')
+    phd_courses = PhdCourses.objects.all().order_by("id")
+    return render(request, 'phd_courses.html', {'phd_courses' : phd_courses})
