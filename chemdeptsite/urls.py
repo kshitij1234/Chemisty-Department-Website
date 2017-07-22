a771d8c34181ce1f4a9f105b93165a1e2e9c2466
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-07-22 15:03:07 +0530

    made all urls case insensitive

--- chemdeptsite/urls.py
+++ chemdeptsite/urls.py
@@ -18,22 +18,22 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name="home"),
-    url(r'^news/$', views.news, name="news"),
-    url(r'^news/(?P<pk>\d+)$', views.individual_news, name="individual_news.html"),
-    url(r'^admissions/postgraduate/$', views.admissions_postgraduate, name="postgraduate_admissions"),
-    url(r'^admissions/doctoral/$', views.admissions_doctoral, name="doctoral_admissions"),
-    url(r'^admissions/FAQ/$', views.admissions_faq, name="faq_admissions"),
-    url(r'^sitemap/$', views.sitemap, name="sitemap"),
-    url(r'^head-message/$', views.head_msg, name="head_msg"),
-    url(r'^head-profile/$', views.head_profile, name="head_profile"),
-    url(r'^facilities/$', views.facilities, name="facilities"),
-    url(r'^directory/', include('directory.urls')),
-    url(r'^people/', include('PeopleApp.urls')),
-    url(r'^events/', include('Events.urls')),
-    url(r'^research/', include('research.urls')),
-    url(r'^academics/', include('courses.urls')),
+    url(r'^(?i)admin/', admin.site.urls),
+    url(r'^(?i)$', views.index, name="home"),
+    url(r'^(?i)news/$', views.news, name="news"),
+    url(r'^(?i)news/(?P<pk>\d+)$', views.individual_news, name="individual_news.html"),
+    url(r'^(?i)admissions/postgraduate/$', views.admissions_postgraduate, name="postgraduate_admissions"),
+    url(r'^(?i)admissions/doctoral/$', views.admissions_doctoral, name="doctoral_admissions"),
+    url(r'^(?i)admissions/FAQ/$', views.admissions_faq, name="faq_admissions"),
+    url(r'^(?i)sitemap/$', views.sitemap, name="sitemap"),
+    url(r'^(?i)head-message/$', views.head_msg, name="head_msg"),
+    url(r'^(?i)head-profile/$', views.head_profile, name="head_profile"),
+    url(r'^(?i)facilities/$', views.facilities, name="facilities"),
+    url(r'^(?i)directory/', include('directory.urls')),
+    url(r'^(?i)people/', include('PeopleApp.urls')),
+    url(r'^(?i)events/', include('Events.urls')),
+    url(r'^(?i)research/', include('research.urls')),
+    url(r'^(?i)academics/', include('courses.urls')),
 
 
     # please keep this url at the end, this needs to be last in the list
--- directory/urls.py
+++ directory/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.directory, name='directory'),
+    url(r'^(?i)$', views.directory, name='directory'),
 ]
