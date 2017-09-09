c31b644070b2f511d189775f0389049f7c7b2957
Abhishek Agrawal <aagrawal207@gmail.com>
2017-09-09 17:39:07 +0530

    URLs are fully responsive now

--- PeopleApp/urls.py
+++ PeopleApp/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)faculty/$', views.faculty_list, name='faculty_list'),
+    url(r'^(?i)faculty/*$', views.faculty_list, name='faculty_list'),
     url(r'^(?i)faculty/(?P<pk>.+)$', views.individual_profile, name="individual_profile.html"),
-    url(r'^(?i)staff/$', views.staff_list, name='staff_list'),
-    url(r'^(?i)students/UG$', views.undergraduate_list, name="undergraduate_list"),
-    url(r'^(?i)students/Msc$', views.msc_list, name="msc_list"),
-    url(r'^(?i)students/Phd$', views.phd_list, name='phd_list'),
-    url(r'^(?i)students/PhdAlumni$', views.phd_alumni_list, name='phd_alumni_list'),
+    url(r'^(?i)staff/*$', views.staff_list, name='staff_list'),
+    url(r'^(?i)students/UG/*$', views.undergraduate_list, name="undergraduate_list"),
+    url(r'^(?i)students/Msc/*$', views.msc_list, name="msc_list"),
+    url(r'^(?i)students/Phd/*$', views.phd_list, name='phd_list'),
+    url(r'^(?i)students/PhdAlumni/*$', views.phd_alumni_list, name='phd_alumni_list'),
 ]
--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)btech/$', views.btech_courses, name="btech_courses"),
-    url(r'^(?i)msc/$', views.msc_courses, name="msc_courses"),
-    url(r'^(?i)phd/$', views.phd_courses, name="phd_courses"),
+    url(r'^(?i)btech/*$', views.btech_courses, name="btech_courses"),
+    url(r'^(?i)msc/*$', views.msc_courses, name="msc_courses"),
+    url(r'^(?i)phd/*$', views.phd_courses, name="phd_courses"),
 ]
