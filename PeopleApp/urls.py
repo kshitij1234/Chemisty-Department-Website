08253525e790a60cd74aca96406eff4686e401aa
Abhishek Agrawal <aagrawal207@gmail.com>
2017-09-09 18:11:11 +0530

    urls are more responsive

--- PeopleApp/urls.py
+++ PeopleApp/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)faculty/$', views.faculty_list, name='faculty_list'),
     url(r'^(?i)faculty/(?P<pk>.+)$', views.individual_profile, name="individual_profile.html"),
+    url(r'^(?i)faculty/*$', views.faculty_list, name='faculty_list'),
     url(r'^(?i)staff/*$', views.staff_list, name='staff_list'),
     url(r'^(?i)students/UG/*$', views.undergraduate_list, name="undergraduate_list"),
     url(r'^(?i)students/Msc/*$', views.msc_list, name="msc_list"),
