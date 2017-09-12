c68aa882bab473391120b25d62a08779d4f8caca
Abhishek Agrawal <aagrawal207@gmail.com>
2017-09-12 12:17:18 +0530

    Better url integration

--- research/urls.py
+++ research/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)projects/*$', views.projects, name='projects'),
-    url(r'^(?i)areas/*$', views.areas, name='areas'),
-    url(r'^(?i)publications/*$', views.publications, name='publications'),
+    url(r'^(?i)projects/?$', views.projects, name='projects'),
+    url(r'^(?i)areas/?$', views.areas, name='areas'),
+    url(r'^(?i)publications/?$', views.publications, name='publications'),
 ]
