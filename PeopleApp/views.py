d86d33fbc1ad0a5416cfbe6153fea857b1e6d72e
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-11-09 16:02:48 +0530

    completed the extra year addition for students

--- PeopleApp/views.py
+++ PeopleApp/views.py
@@ -22,20 +22,40 @@ def undergraduate_list(request):
     students13 = UndergraduateStudents.objects.filter(batch__batch__contains="2013").order_by('rollno')
     students14 = UndergraduateStudents.objects.filter(batch__batch__contains="2014").order_by('rollno')
     students15 = UndergraduateStudents.objects.filter(batch__batch__contains="2015").order_by('rollno')
-    # students16 = UndergraduateStudents.objects.filter(batch__batch__contains="2016").order_by('rollno')
+    students16 = UndergraduateStudents.objects.filter(batch__batch__contains="2016").order_by('rollno')
+    students17 = UndergraduateStudents.objects.filter(batch__batch__contains="2017").order_by('rollno')
+    students18 = UndergraduateStudents.objects.filter(batch__batch__contains="2018").order_by('rollno')
+    students19 = UndergraduateStudents.objects.filter(batch__batch__contains="2019").order_by('rollno')
+    students20 = UndergraduateStudents.objects.filter(batch__batch__contains="2020").order_by('rollno')
     return render(request, 'PeopleApp/UndergraduateList.html', {'students13': students13,
                                                                 'students14': students14,
                                                                 'students15': students15,
-                                                                #             'students16': students16
+                                                                'students16': students16,
+                                                                'students17': students17,
+                                                                'students18': students18,
+                                                                'students19': students19,
+                                                                'students20': students20,
                                                                 })
 
 
 def msc_list(request):
     students16 = MscStudents.objects.filter(batch__batch__contains="2016").order_by('rollno')
-    return render(request, 'PeopleApp/MscList.html', {'students16': students16})
+    students17 = MscStudents.objects.filter(batch__batch__contains="2017").order_by('rollno')
+    students18 = MscStudents.objects.filter(batch__batch__contains="2018").order_by('rollno')
+    students19 = MscStudents.objects.filter(batch__batch__contains="2019").order_by('rollno')
+    students20 = MscStudents.objects.filter(batch__batch__contains="2020").order_by('rollno')
+    return render(request, 'PeopleApp/MscList.html', {'students16': students16,
+                                                      'students17': students17,
+                                                      'students18': students18,
+                                                      'students19': students19,
+                                                      'students20': students20,
+                                                      })
 
 
 def phd_list(request):
+    students20 = PhdStudents.objects.filter(batch__batch__contains="2020").order_by('name')
+    students19 = PhdStudents.objects.filter(batch__batch__contains="2019").order_by('name')
+    students18 = PhdStudents.objects.filter(batch__batch__contains="2018").order_by('name')
     students17 = PhdStudents.objects.filter(batch__batch__contains="2017").order_by('name')
     students16 = PhdStudents.objects.filter(batch__batch__contains="2016").order_by('name')
     students15 = PhdStudents.objects.filter(batch__batch__contains="2015").order_by('name')
@@ -51,7 +71,11 @@ def phd_list(request):
                                                       'students13': students13,
                                                       'students11': students11,
                                                       'students10': students10,
-                                                      'students9': students9})
+                                                      'students9': students9,
+                                                      'students18': students18,
+                                                      'students19': students19,
+                                                      'students20': students20,
+                                                      })
 
 
 def phd_alumni_list(request):
